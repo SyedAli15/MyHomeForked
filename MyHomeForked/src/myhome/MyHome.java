688906bc70cd009ef83ad77d1dc5ff67bbd30a64
SyedAli15 <syounusa@gmail.com>
2022-11-25 21:38:38 +0000

    Address Class added.

---
+++ MyHomeForked/src/myhome/MyHome.java
@@ -0,0 +1,13 @@
+package myhome;
+
+public class MyHome {
+
+	public static void main(String[] args) {
+		String address;
+		address="Flat 1 High Street, London";
+		
+		System.out.println(address);
+
+	}
+
+}
